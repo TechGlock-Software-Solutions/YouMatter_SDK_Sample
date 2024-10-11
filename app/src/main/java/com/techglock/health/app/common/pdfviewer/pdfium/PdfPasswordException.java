48c844e128efb8e7f8a11923d88fc5bebc3e1123
rajan1141 <akshay.khajuria@techglock.com>
2024-10-11 19:03:38 +0530

    update deprecated dependency

---
+++ app/src/main/java/com/techglock/health/app/common/pdfviewer/pdfium/PdfPasswordException.java
@@ -0,0 +1,13 @@
+package com.techglock.health.app.common.pdfviewer.pdfium;
+
+import java.io.IOException;
+
+public class PdfPasswordException extends IOException {
+    public PdfPasswordException() {
+        super();
+    }
+
+    public PdfPasswordException(String detailMessage) {
+        super(detailMessage);
+    }
+}
